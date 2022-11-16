0db575602faf127558e38ed4f52a274eda0f2c3e
JaredzG <82556918+JaredzG@users.noreply.github.com>
2022-11-15 22:06:16 -0600

    Update appointment.inc.php
    doctor id intval

--- includes/appointment.inc.php
+++ includes/appointment.inc.php
@@ -2,10 +2,9 @@
 session_start();
 if (isset($_POST["submit"])) {
 	$date = $_POST["adate-time"];
-  $doctor = $_POST["doctor"];
+  $doctor = intval($_POST["doctor"]);
   $reason = $_POST["reason"];
   $username = $_POST["username"];
-  
   require_once "dbh.inc.php";
   require_once "functions.inc.php";
   
