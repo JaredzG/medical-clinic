2ad643c901383d67d0d33f161fc00938f66fd1eb
cbaticados <49915726+cbaticados@users.noreply.github.com>
2022-11-16 17:13:09 -0600

    Updated users.php
    fixed the while loop condition when generating rows

--- users.php
+++ users.php
@@ -12,74 +12,79 @@
 ?>
 <div class='choose-user'>
   <h2>Choose a User</h2>
-  <?php
-    while ($row = mysqli_fetch_assoc($result)) {
-      $userID = $row["user_ID"];
-      $userRole = $row["user_role"];
-      $result2 = getUserInfo($conn, $userID, $userRole);
-      if ($result2 !== 'admin') {
-        $row2 = mysqli_fetch_assoc($result2);
-        $fname = $row2["f_name"];
-        $lname = $row2["l_name"];
-      }
-  ?>
-  <table>
-    <tr>
-      <th>User ID</th>
-      <th>User Role</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Action</th>
-    </tr>
-    <tr>
-      <td><?php echo $userID?></td>
-      <td><?php echo $userRole?></td>
-      <td><?php echo $fname?></td>
-      <td><?php echo $lname?></td>
-      <td>
-      <?php if ($_SESSION["userRole"] === 'admin') {
-        if ($userRole !== 'admin') {
-      ?>
-        <form action='/medical-clinic/viewinfo.php' method='post'>
-          <div class='form-element'>
-            <input style='display: none;' name='userID' value='<?php echo $userID?>'/>
-          </div>
-          <div class='form-element'>
-            <input style='display: none;' name='userRole' value='<?php echo $userRole?>'/>
-          </div>
-          <div class='submit-btn'>
-            <button type='submit' name='submit'>View Info</button>
-          </div>
-        </form>
+  <table class = "table-template">
+    <thead>
+      <tr>
+        <th>User ID</th>
+        <th>User Role</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+
+    <tbody>
       <?php
+      while ($row = mysqli_fetch_assoc($result)) {
+        $userID = $row["user_ID"];
+        $userRole = $row["user_role"];
+        $result2 = getUserInfo($conn, $userID, $userRole);
+        if ($result2 !== 'admin') {
+          $row2 = mysqli_fetch_assoc($result2);
+          $fname = $row2["f_name"];
+          $lname = $row2["l_name"];
         }
       ?>
-        <form action='/medical-clinic/delete.php' method='post'>
-          <div class='form-element'>
-            <input style='display: none;' name='userID' value='<?php echo $userID?>'/>
-          </div>
-          <div class='form-element'>
-            <input style='display: none;' name='userRole' value='<?php echo $userRole?>'/>
-          </div>
-          <div class='form-element'>
-            <input style='display: none;' name='otherID' value='none'/>
-          </div>
-          <div class='form-element'>
-            <input style='display: none;' name='otherRole' value='none'/>
-          </div>
-          <div class='form-element'>
-            <input style='display: none;' name='senderRole' value='admin'/>
-          </div>
-          <div class='submit-btn'>
-            <button type='submit' name='submit'>Delete</button>
-          </div>
-        </form>
+      <tr>
+        <td><?php echo $userID?></td>
+        <td><?php echo $userRole?></td>
+        <td><?php echo $fname?></td>
+        <td><?php echo $lname?></td>
+        <td>
+        <?php if ($_SESSION["userRole"] === 'admin') {
+          if ($userRole !== 'admin') {
+        ?>
+          <form action='/medical-clinic/viewinfo.php' method='post'>
+            <div class='form-element'>
+              <input style='display: none;' name='userID' value='<?php echo $userID?>'/>
+            </div>
+            <div class='form-element'>
+              <input style='display: none;' name='userRole' value='<?php echo $userRole?>'/>
+            </div>
+            <div class='submit-btn'>
+              <button type='submit' name='submit'>View Info</button>
+            </div>
+          </form>
+        <?php
+          }
+        ?>
+          <form action='/medical-clinic/delete.php' method='post'>
+            <div class='form-element'>
+              <input style='display: none;' name='userID' value='<?php echo $userID?>'/>
+            </div>
+            <div class='form-element'>
+              <input style='display: none;' name='userRole' value='<?php echo $userRole?>'/>
+            </div>
+            <div class='form-element'>
+              <input style='display: none;' name='otherID' value='none'/>
+            </div>
+            <div class='form-element'>
+              <input style='display: none;' name='otherRole' value='none'/>
+            </div>
+            <div class='form-element'>
+              <input style='display: none;' name='senderRole' value='admin'/>
+            </div>
+            <div class='submit-btn'>
+              <button type='submit' name='submit'>Delete</button>
+            </div>
+          </form>
+        <?php }?>
+        </td>
+      </tr>
       <?php }?>
-      </td>
-    </tr>
+    </tbody>
   </table>
 </div>
 <?php
-    }
   include_once 'footer.php';
 ?>
